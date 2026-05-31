acb1b7c833582665175f3cf8d4faeb0dacf0e5f6
JustKeisokki <keisokki.anw@gmail.com>
2026-05-31 18:15:50 +0700

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // 💡 1. Import library Swagger-nya di sini
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common'; // 🆕 TAMBAHKAN IMPORT INI
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 🔓 KONFIGURASI CORS AMAN: Mengunci akses hanya untuk frontend localhost:3000
+  // 🔓 KONFIGURASI CORS AMAN: Mengunci akses hanya untuk frontend localhost:3001
   app.enableCors({
-    origin: 'http://localhost:3000', // Hanya alamat ini yang diizinkan mengakses backend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Metode HTTP yang diperbolehkan
-    credentials: true, // Izinkan jika frontend mengirimkan cookie/header otentikasi
+    origin: 'http://localhost:3001', // Hanya alamat ini yang diizinkan mengakses backend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
+
+  // 🆕 TAMBAHKAN BARIS INI agar data request body dari DTO tidak bernilai undefined lagi!
+  app.useGlobalPipes(new ValidationPipe());
 
   // 💡 2. Nyalakan dan Atur Konfigurasi Swagger
   const config = new DocumentBuilder()
