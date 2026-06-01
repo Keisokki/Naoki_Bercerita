e1df932cb4e311461e40fdb4d2d1ab9957a4b09e
JustKeisokki <keisokki.anw@gmail.com>
2026-06-01 19:36:39 +0700

    Add files via upload

--- src/table/dto/table.dto.ts
+++ src/table/dto/table.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator'; // 🆕 IMPORT INI
 
 export class CreateTableDto {
   @ApiProperty({ example: 'A1', description: 'Nomor atau kode identitas meja restoran' })
+  @IsNotEmpty({ message: 'Nomor meja tidak boleh kosong!' }) // 🆕 Validasi wajib diisi
+  @IsString({ message: 'Nomor meja harus berupa teks string!' }) // 🆕 Validasi bertipe string
   number!: string;
 }
 
@@ -11,5 +14,6 @@ export class UpdateTableStatusDto {
     description: 'Status keterisian meja makan',
     enum: ['AVAILABLE', 'OCCUPIED'] 
   })
+  @IsNotEmpty({ message: 'Status meja tidak boleh kosong!' }) // 🆕 Validasi wajib diisi
   status!: 'AVAILABLE' | 'OCCUPIED';
 }
