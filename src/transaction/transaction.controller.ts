1caf250e1b49241b76a7584f03b1472d0be0a13f
Keisokki <keisokki_wijaya_33rpl@student.smktelkom-mlg.sch.id>
2026-06-03 08:04:18 +0700

    Add files via upload

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -4,7 +4,7 @@ import { Roles } from '../auth/roles.decorator';
 import { RolesGuard } from '../auth/roles.guard';
 import { AuthGuard } from '../auth/auth.guard';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { CheckoutDto, PaymentDto } from './dto/transaction.dto'; // 💡 Memastikan import DTO lengkap
+import { CheckoutDto, PaymentDto } from './dto/transaction.dto';
 
 @ApiTags('6. Inti Kasir & Transaksi')
 @Controller('transaction')
@@ -14,10 +14,9 @@ export class TransactionController {
   constructor(private transactionService: TransactionService) {}
 
   @Post('checkout')
-  @Roles('KASIR')
-  @ApiOperation({ summary: '🛍️ [KASIR ONLY] Membuat pesanan baru pelanggan (Auto kunci meja & potong stok)' })
+  @Roles('KASIR', 'ADMIN') 
+  @ApiOperation({ summary: '🛍️ [KASIR / ADMIN] Membuat pesanan baru pelanggan (Auto kunci meja & potong stok)' })
   async checkout(@Body() body: CheckoutDto) {
-    // 🆕 Sekarang diikat menggunakan CheckoutDto agar memunculkan skema JSON "quantity" di Swagger UI
     return this.transactionService.create(body);
   }
 
@@ -42,7 +41,6 @@ export class TransactionController {
   @Roles('KASIR', 'ADMIN')
   @ApiOperation({ summary: '❌ [KASIR / ADMIN] Membatalkan pesanan belum lunas & otomatis mengosongkan meja' })
   async cancelTransaction(@Param('id', ParseIntPipe) id: number) {
-    // Diperbaiki menggunakan ParseIntPipe agar aman berupa tipe data angka murni
     return this.transactionService.cancelTransaction(id);
   }
 
